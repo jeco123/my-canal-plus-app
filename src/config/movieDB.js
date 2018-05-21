72373cabcdbc62c13c1d9042532c140c2fb9be4c
COLLOMP Jerome <collomp_j@yahoo.fr>
2018-05-21 02:02:35 +0200

    feat(redux)

---
+++ src/config/movieDB.js
@@ -0,0 +1,5 @@
+const movieDB = {
+  apiKey: '92b418e837b833be308bbfb1fb2aca1e',
+}
+
+export default movieDB
--- src/reducers/genres.js
+++ src/reducers/genres.js
@@ -1,15 +1,25 @@
-const LOAD_GENRES_SUCCESS = 'LOAD_GENRES_SUCCESS'
+import {
+  FETCH_GENRES_SUCCESS,
+  FETCH_GENRES_LOADING,
+} from '../actions/genres';
 
 const initialState = {
   genres: [],
+  isLoading: false,
 };
 
 export const genres = (state = initialState, action) => {
   switch (action.type) {
-    case LOAD_GENRES_SUCCESS:
+    case FETCH_GENRES_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case FETCH_GENRES_SUCCESS:
       return {
         ...state,
         genres: action.payload,
+        isLoading: false,
       }
     default:
       return state
